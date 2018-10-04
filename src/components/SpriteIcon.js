a120e547ee62b145435cbcbc143b8b053422555d
Angel-Barena <angelbarena89@hotmail.com>
2018-10-04 00:00:35 -0500

    fixed svg sizing error

--- src/components/SpriteIcon.js
+++ src/components/SpriteIcon.js
@@ -4,8 +4,8 @@ class SpriteIcon extends Component {
     render() {
         const { svgIcon, color} = this.props;
         return (
-            <svg viewBox={ svgIcon.viewBox } className="tech-list__item__icon" height="3.6rem">
-                <use xlinkHref={ svgIcon.url } height="3.6rem"/>
+            <svg viewBox={svgIcon.viewBox} className="tech-list__item__icon">
+                <use xlinkHref={svgIcon.url}/>
             </svg>
         );
     }
